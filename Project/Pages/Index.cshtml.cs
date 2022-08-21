a3d02c28a718d5f0bd4025bf2754e1373b7684d3
Tomasz Krzemiński <7beat123@wp.pl>
2022-08-21 15:29:39 +0200

    Removed unused code

--- Project/Pages/Index.cshtml.cs
+++ Project/Pages/Index.cshtml.cs
@@ -13,7 +13,6 @@ namespace Project.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ApplicationDbContext _context;
-        //Będzie działać podobnie!
 
         public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
@@ -24,14 +23,13 @@ namespace Project.Pages
         }
 
         [BindProperty]
-        public Person NewPerson { get; set; }
-
-        [BindProperty]//NewContact?
         public Contact Contact { get; set; }
-        //Person template used to create objects
+        //Template used to create objects
 
         public List<Person> People = new();
         public List<Contact> Contacts = new();
+
+        //public List<Contact> Contacts2; //null testing
 
         public string data;
 
@@ -39,12 +37,13 @@ namespace Project.Pages
         {
             try
             {
-                Contacts = _context.Contacts.ToList();
+                Contacts = _context.Contacts.ToList();//Count == 0(czyli nie jest nullem tylko ma 0 tabel), here init? else null?
+                //Contacts2 = new(_context.Contacts.ToList()); //to jest teraz nullem! Jak działa to nie jest nullem! Site throws exception, DbContext not working!
+                //Contacts = new(_context.Contacts.ToList());
             }
             catch (Exception e)
             {
                 _logger.LogCritical(e, e.Message);
-                return;
             }
         }
 
@@ -85,7 +84,7 @@ namespace Project.Pages
             }
         }
 
-        private void DelContactDB(int id) //Works!
+        private void DelContactDB(int id)
         {
             var contact = _context.Contacts.FirstOrDefault(x => x.Id == id);
 
@@ -97,113 +96,6 @@ namespace Project.Pages
             catch (Exception e)
             {
                 _logger.LogError(e, e.Message);
-                //throw;
-            }
-        }
-
-        private void AddNewPersonDB(Person person)
-        {
-            string conString = "SERVER=localhost;DATABASE=project;UID=root;";
-
-            var query = $@"INSERT INTO `users` (`id`, `email`, `password`, `name`, `surname`, `phone`, `birthday`, `category`) VALUES (NULL, '{person.Email}', 'user', '{person.FirstName}', '{person.LastName}', '{person.Phone}', '{person.BirthDay.ToString("O")}', '{person.Category}')";
-
-            MySqlConnection conn = new MySqlConnection(conString);
-
-            //Musi dostać obiekt typu contact
-            //_context.Add<Contact>();
-
-            try
-            {
-                conn.Open();
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Critical, "Couldnt connect to DB, Error Message: " + e.Message);
-                return;
-            }
-
-            var cmd = new MySqlCommand(query, conn);
-
-            try
-            {
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Critical, "Couldnt not execute query, Error Message: " + e.Message);
-            }
-        }
-
-        private void ConnectDB()
-        {
-
-            string conString = "SERVER=localhost;DATABASE=project;UID=root;";
-
-            MySqlConnection conn = new MySqlConnection(conString);
-
-            try
-            {
-                _logger.Log(LogLevel.Information, "Connecting to DataBase");
-                conn.Open();
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Critical, "Couldnt connect to DB, Error Message: " + e.Message);
-                return;
-            }
-
-            string query = "SELECT * FROM users";
-
-            using (var cmd = new MySqlCommand(query, conn))
-            {
-                using (var rdr = cmd.ExecuteReader())
-                {
-                    while (rdr.Read())
-                    {
-                        var category = rdr["category"].ToString();
-                        Console.WriteLine(category);
-                        People.Add(new Person()
-                        {
-                            Id = Convert.ToInt32(rdr["id"]),
-                            FirstName = rdr["name"].ToString(),
-                            LastName = rdr["surname"].ToString(),
-                            BirthDay = Convert.ToDateTime(rdr["birthday"]),
-                            Email = rdr["email"].ToString(),
-                            Phone = rdr["phone"].ToString()
-                        });
-                    }
-                }
-            }
-        }
-
-        private void DeletePersonDB(int id)
-        {
-            string conString = "SERVER=localhost;DATABASE=project;UID=root;";
-
-            string query = $"DELETE FROM users WHERE `users`.`id` = {id}";
-
-            MySqlConnection conn = new MySqlConnection(conString);
-
-            try
-            {
-                conn.Open();
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Critical, "Couldnt connect to DB, Error Message: " + e.Message);
-                return;
-            }
-
-            var cmd = new MySqlCommand(query, conn);
-
-            try
-            {
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Critical, "Couldnt execute SQL Query, Error Message: " + e.Message);
-                return;
             }
         }
     }
