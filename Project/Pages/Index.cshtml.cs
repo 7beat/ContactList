e8c2998e638e60c65ec441212dbb3b2a5496b518
Tomasz Krzemiński <7beat123@wp.pl>
2022-08-21 12:58:10 +0200

    Added Js popup template

--- Project/Pages/Index.cshtml.cs
+++ Project/Pages/Index.cshtml.cs
@@ -37,54 +37,71 @@ namespace Project.Pages
 
         public void OnGet()
         {
-            Contacts = _context.Contacts.ToList();
-
-            //foreach (var item in Contacts)
-            //{
-            //    Console.WriteLine($"Contacts: {item.FirstName}");
-            //}
-            //Refresh Data EveryTime
+            try
+            {
+                Contacts = _context.Contacts.ToList();
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e, e.Message);
+                return;
+            }
         }
-
 
         public IActionResult OnPost()
         {
-            Console.WriteLine(Contact.FirstName); 
+            Console.WriteLine(Contact.FirstName);
             Console.WriteLine(Contact.LastName);
             Console.WriteLine(Contact.Email);
             Console.WriteLine($"Contact's BirthDay: {Contact.BirthDay}");
             //LOG: Contact's BirthDay: 01.01.0001, po wybraniu daty z formy
-            //Contact został uzupełniony z formy!
+            //ToDo: Fix Birth date!
 
-            //Contact's id is specified from form!
-            if (NewPerson.Id == 0) //If 0 means it was sent from creation form else it was specified to delete
+            if (Contact.Id == 0) //If 0 means it was sent from creation form else it was specified to delete
             {
-                //AddContactDB(Contact);
-                //AddNewPersonDB(new() { FirstName = NewPerson.FirstName, LastName = NewPerson.LastName, Email = NewPerson.Email, Phone = NewPerson.Phone, BirthDay = NewPerson.BirthDay, Category = NewPerson.Category });
-
+                AddContactDB(Contact);
             }
             else
             {
-                //DeletePersonDB(NewPerson.Id);
-                //DeleteContactDB();
-
+                DelContactDB(Contact.Id);
             }
 
             return RedirectToPage("Index");
             //Refresh
         }
 
-        private void AddContactDB(Contact newContact)
+        private void AddContactDB(Contact newContact) //Works!
         {
-            //try? date format. ToDo: Fix date!
-            //_context.Add(contact);
-            _context.Contacts.Add(newContact);
-            _context.SaveChanges();
-            //Musi być w try bo jeśli wartośc unique się powtarza to exception!
-            //SaveChanges() to insert it!
+            //ToDo: Fix Date!
+            try
+            {
+                _context.Contacts.Add(newContact);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                //ToDo: PopUP
+                _logger.LogError(e, e.InnerException.Message);
+            }
         }
 
-        private void AddNewPersonDB(Person person) 
+        private void DelContactDB(int id) //Works!
+        {
+            var contact = _context.Contacts.FirstOrDefault(x => x.Id == id);
+
+            try
+            {
+                _context.Contacts.Remove(contact);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                //throw;
+            }
+        }
+
+        private void AddNewPersonDB(Person person)
         {
             string conString = "SERVER=localhost;DATABASE=project;UID=root;";
 
