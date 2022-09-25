8d6bde2e4dc5dfdcf2056b453133a03d2b94bdf8
Tomasz Krzemiński <7beat123@wp.pl>
2022-09-25 17:25:41 +0200

    Migrations

---
+++ Project/Migrations/20220925151821_NewBday.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    public partial class NewBday : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "BirthDay",
+                table: "Contacts",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1,1,1));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDay",
+                table: "Contacts");
+        }
+    }
+}
